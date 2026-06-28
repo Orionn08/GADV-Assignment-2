cc24c0800f9f045293f7e6e474473c7e121f6871
Orionn08 <blueknightrider69@gmail.com>
2026-06-28 14:00:59 +0800

    Got room highlights to work
    switched from using OnMouseEnter() and OnMouseExit() to determine if the cursor is over a room slot to raycast. Made a room placeholder for my next step which is placing a room by clicking.

--- Assets/Scripts/RoomSlot.cs
+++ Assets/Scripts/RoomSlot.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RoomSlot : MonoBehaviour
 {
     [SerializeField] private GameObject _highlight;
 
-    void OnMouseEnter() {
-        _highlight.SetActive(true);
-    }
- 
-    void OnMouseExit()
+    void Update()
     {
-        _highlight.SetActive(false);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            _highlight.SetActive(true);
+        }
+        else
+        {
+            _highlight.SetActive(false);
+        }
     }
 }
